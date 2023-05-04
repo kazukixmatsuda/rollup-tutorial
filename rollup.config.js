969df43fe394d4a85e02a9f735045a31bfc94ec9
kazukixmatsuda <xm.kazuki827@gmail.com>
2023-05-04 14:17:18 +0900

    add json plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+const { default: json } = require('@rollup/plugin-json');
+
 module.exports = {
   input: 'src/main.js',
   output: {
     file: 'bundle.js',
     format: 'cjs'
-  }
+  },
+  plugins: [json()]
 };
